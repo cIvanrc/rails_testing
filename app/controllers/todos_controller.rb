fb6d737f698b73c308c9a90da6fec78c2facbad2
ivan <c.ivan.felix.r@gmail.com>
2019-05-15 13:20:08 -0500

    refactor on model user

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -2,7 +2,7 @@ class TodosController < ApplicationController
   before_action :authenticate
 
   def index
-    @todos = Todo.where(email: session[:current_email])
+    @todos = current_user.todos
   end
 
   def new
@@ -10,13 +10,14 @@ class TodosController < ApplicationController
   end
 
   def create
-    Todo.create(todos_params.merge(email: session[:current_email]))
+    current_user.todos.create(todo_params)
+    Todo.create(todo_params.merge(email: session[:current_email]))
     redirect_to todos_path
   end
 
   private
 
-  def todos_params
+  def todo_params
     params.require(:todo).permit(:title)
   end
 end
